f0e6572547c29e146f5c24ecf2538d868a5eb735
Jasmine Hurst <jasmine.ehurst@gmail.com>
2016-06-20 14:59:27 -0500

    Initial commit

---
+++ NaivePatternSearching/src/SearchString.java
@@ -0,0 +1,28 @@
+
+public class SearchString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String search = "sssyttyssyttysyts";
+		String pattern = "tt";
+		searchString(pattern, search);
+	}
+
+	public static void searchString(String pattern, String string){
+		if(pattern.length() > string.length()){
+			return;
+		}
+		for(int i = 0; i <= string.length() 
+				- pattern.length(); i++){
+			for(int j = 0; j < pattern.length(); j++){
+				if(string.charAt(i+j) != pattern.charAt(j)){
+					break;
+				}
+				if(j + 1 == pattern.length()){
+					System.out.println("Index found at " + i);
+				}
+			}
+		}
+		return;
+	}
+}
